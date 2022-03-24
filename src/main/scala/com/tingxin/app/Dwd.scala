533c43be428124255c98a9e65b0b4789d37a6c48
barry <friendship-119@163.com>
2022-03-24 16:02:11 +0800

    init

---
+++ src/main/scala/com/tingxin/app/Dwd.scala
@@ -0,0 +1,64 @@
+package com.tingxin.app
+
+import com.tingxin.entity.Item
+import org.apache.spark.sql.functions.array_contains
+import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
+import org.apache.spark.sql.{Row, SparkSession}
+import com.amazon.deequ.VerificationSuite
+import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
+import com.amazon.deequ.constraints.ConstraintStatus
+
+object Dwd extends App{
+
+  val spark: SparkSession = SparkSession.builder()
+    .master("local[1]")
+    .appName("SparkByExamples.com")
+    .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  val rdd = spark.sparkContext.parallelize(Seq(
+    Item(1, "Thingy A", "awesome thing.", "high", 0),
+    Item(2, "Thingy B", "available at http://thingb.com", null, 0),
+    Item(3, null, null, "low", 5),
+    Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
+    Item(5, "Thingy E", null, "high", 12)))
+
+  val data = spark.createDataFrame(rdd)
+
+  data.show(10)
+
+  val verificationResult = VerificationSuite()
+    .onData(data)
+    .addCheck(
+      Check(CheckLevel.Error, "unit testing my data")
+        .hasSize(_ == 5) // we expect 5 rows
+        .isComplete("id") // should never be NULL
+        .isUnique("id") // should not contain duplicates
+        .isComplete("productName") // should never be NULL
+        // should only contain the values "high" and "low"
+        .isContainedIn("priority", Array("high", "low"))
+        .isNonNegative("numViews") // should not contain negative values
+        // at least half of the descriptions should contain a url
+        .containsURL("description", _ >= 0.5)
+        // half of the items should have less than 10 views
+        .hasApproxQuantile("numViews", 0.5, _ <= 10))
+    .run()
+
+
+
+  if (verificationResult.status == CheckStatus.Success) {
+    println("The data passed the test, everything is fine!")
+  } else {
+    println("Dwd data quality:\n")
+
+    val resultsForAllConstraints = verificationResult.checkResults
+      .flatMap { case (_, checkResult) => checkResult.constraintResults }
+
+    resultsForAllConstraints
+      .filter { _.status != ConstraintStatus.Success }
+      .foreach { result => println(s"${result.constraint}: ${result.message.get}") }
+  }
+
+}
+
---
+++ src/main/scala/com/tingxin/entity/Item.scala
@@ -0,0 +1,9 @@
+package com.tingxin.entity
+
+case class Item(
+                 id: Long,
+                 productName: String,
+                 description: String,
+                 priority: String,
+                 numViews: Long
+               )
