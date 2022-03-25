1014c779c54f6e488495e7a88b45232ed7813f67
EC2 Default User <ec2-user@ip-172-31-43-238.cn-northwest-1.compute.internal>
2022-03-25 05:30:15 +0000

    update

--- src/main/scala/com/tingxin/app/Dwd.scala
+++ src/main/scala/com/tingxin/app/Dwd.scala
@@ -1,5 +1,6 @@
 package com.tingxin.app
 
+import org.apache.spark._
 import com.tingxin.entity.Item
 import org.apache.spark.sql.functions.array_contains
 import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
@@ -8,22 +9,22 @@ import com.amazon.deequ.VerificationSuite
 import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
 import com.amazon.deequ.constraints.ConstraintStatus
 
-object Dwd extends App{
-
-  val spark: SparkSession = SparkSession.builder()
-    .master("yarn")
-//    .config("spark.driver.bindAddress", "127.0.0.1")
+/** Computes an approximation to pi */
+object Dwd extends App {
+  val spark: SparkSession = SparkSession
+    .builder()
     .appName("com.tingxin")
     .getOrCreate()
 
-  spark.sparkContext.setLogLevel("ERROR")
-
-  val rdd = spark.sparkContext.parallelize(Seq(
-    Item(1, "Thingy A", "awesome thing.", "high", 0),
-    Item(2, "Thingy B", "available at http://thingb.com", null, 0),
-    Item(3, null, null, "low", 5),
-    Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
-    Item(5, "Thingy E", null, "high", 12)))
+  val rdd = spark.sparkContext.parallelize(
+    Seq(
+      Item(1, "Thingy A", "awesome thing.", "high", 0),
+      Item(2, "Thingy B", "available at http://thingb.com", null, 0),
+      Item(3, null, null, "low", 5),
+      Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
+      Item(5, "Thingy E", null, "high", 12)
+    )
+  )
 
   val data = spark.createDataFrame(rdd)
 
@@ -43,23 +44,24 @@ object Dwd extends App{
         // at least half of the descriptions should contain a url
         .containsURL("description", _ >= 0.5)
         // half of the items should have less than 10 views
-        .hasApproxQuantile("numViews", 0.5, _ <= 10))
+        .hasApproxQuantile("numViews", 0.5, _ <= 10)
+    )
     .run()
-
-
 
   if (verificationResult.status == CheckStatus.Success) {
     println("The data passed the test, everything is fine!")
   } else {
-    println("Dwd data quality:\n")
+    println("Dws data quality:\n")
 
     val resultsForAllConstraints = verificationResult.checkResults
       .flatMap { case (_, checkResult) => checkResult.constraintResults }
 
     resultsForAllConstraints
       .filter { _.status != ConstraintStatus.Success }
-      .foreach { result => println(s"${result.constraint}: ${result.message.get}") }
+      .foreach { result =>
+        println(s"${result.constraint}: ${result.message.get}")
+      }
   }
 
+  spark.sparkContext.stop()
 }
-
--- src/main/scala/com/tingxin/app/Dws.scala
+++ src/main/scala/com/tingxin/app/Dws.scala
@@ -8,21 +8,25 @@ import com.amazon.deequ.VerificationSuite
 import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
 import com.amazon.deequ.constraints.ConstraintStatus
 
-object Dws extends App{
+object Dws extends App {
 
-  val spark: SparkSession = SparkSession.builder()
+  val spark: SparkSession = SparkSession
+    .builder()
     .master("yarn")
     .appName("com.tingxin")
     .getOrCreate()
 
   spark.sparkContext.setLogLevel("ERROR")
 
-  val rdd = spark.sparkContext.parallelize(Seq(
-    Item(1, "Thingy A", "awesome thing.", "high", 0),
-    Item(2, "Thingy B", "available at http://thingb.com", null, 0),
-    Item(3, null, null, "low", 5),
-    Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
-    Item(5, "Thingy E", null, "high", 12)))
+  val rdd = spark.sparkContext.parallelize(
+    Seq(
+      Item(1, "Thingy A", "awesome thing.", "high", 0),
+      Item(2, "Thingy B", "available at http://thingb.com", null, 0),
+      Item(3, null, null, "low", 5),
+      Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
+      Item(5, "Thingy E", null, "high", 12)
+    )
+  )
 
   val data = spark.createDataFrame(rdd)
 
@@ -42,10 +46,9 @@ object Dws extends App{
         // at least half of the descriptions should contain a url
         .containsURL("description", _ >= 0.5)
         // half of the items should have less than 10 views
-        .hasApproxQuantile("numViews", 0.5, _ <= 10))
+        .hasApproxQuantile("numViews", 0.5, _ <= 10)
+    )
     .run()
-
-
 
   if (verificationResult.status == CheckStatus.Success) {
     println("The data passed the test, everything is fine!")
@@ -57,8 +60,9 @@ object Dws extends App{
 
     resultsForAllConstraints
       .filter { _.status != ConstraintStatus.Success }
-      .foreach { result => println(s"${result.constraint}: ${result.message.get}") }
+      .foreach { result =>
+        println(s"${result.constraint}: ${result.message.get}")
+      }
   }
-
+  spark.sparkContext.stop()
 }
-
