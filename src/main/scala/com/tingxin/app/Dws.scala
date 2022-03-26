f7f29e3b739260294b4317788eaae2e88eb8c39d
EC2 Default User <ec2-user@ip-172-31-43-238.cn-northwest-1.compute.internal>
2022-03-26 17:16:05 +0000

    update

--- src/main/scala/com/tingxin/app/Dws.scala
+++ src/main/scala/com/tingxin/app/Dws.scala
@@ -4,65 +4,55 @@ import com.tingxin.entity.Item
 import org.apache.spark.sql.functions.array_contains
 import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
-// import com.amazon.deequ.VerificationSuite
-// import com.amazon.deequ.checks.{Check, CheckLevel, CheckStatus}
-// import com.amazon.deequ.constraints.ConstraintStatus
+
+import org.apache.hudi.DataSourceWriteOptions
+import org.apache.spark.sql.SaveMode
+import org.apache.hudi.QuickstartUtils._
+import org.apache.hudi.DataSourceWriteOptions._
+import org.apache.hudi.config.HoodieWriteConfig._
 
 object Dws extends App {
 
   val spark: SparkSession = SparkSession
     .builder()
-    .master("yarn")
+    .enableHiveSupport()
     .appName("com.tingxin")
     .getOrCreate()
 
-  spark.sparkContext.setLogLevel("ERROR")
+  val sourceTbName = "spark_hudi_order_dwd"
+  val targetTbName = "spark_hudi_order_dws"
+  val basePath = "s3://tx-workshop/rongbai/spark/hudi_order_dws1/"
 
-  val rdd = spark.sparkContext.parallelize(
-    Seq(
-      Item(1, "Thingy A", "awesome thing.", "high", 0),
-      Item(2, "Thingy B", "available at http://thingb.com", null, 0),
-      Item(3, null, null, "low", 5),
-      Item(4, "Thingy D", "checkout https://thingd.ca", "low", 10),
-      Item(5, "Thingy E", null, "high", 12)
-    )
+  val df = spark.sql(
+    s"select COUNT(order_id) as order_count," +
+      s"COUNT(DISTINCT user_mail) as user_count," +
+      s"SUM(good_count) as good_count," +
+      s"MAX(ts) as ts," +
+      s"logday from $sourceTbName group by logday"
   )
+  df.show(10)
 
-  val data = spark.createDataFrame(rdd)
+  df.write
+    .format("hudi")
+    .options(getQuickstartWriteConfigs)
+    .option(PRECOMBINE_FIELD.key(), "logday")
+    .option(RECORDKEY_FIELD.key(), "logday")
+    .option(PARTITIONPATH_FIELD.key(), "logday")
+    .option(OPERATION.key(), "upsert")
+    .option("hoodie.table.name", targetTbName)
+    .option("hoodie.datasource.hive_sync.enable", "true")
+    .option("hoodie.datasource.hive_sync.database", "default")
+    .option("hoodie.datasource.hive_sync.table", targetTbName)
+    .option("hoodie.datasource.hive_sync.mode", "HMS")
+    .option("hoodie.datasource.hive_sync.use_jdbc", "false")
+    .option("hoodie.datasource.hive_sync.username", "hadoop")
+    .option("hoodie.datasource.hive_sync.partition_fields", "logday")
+    .option(
+      "hoodie.datasource.hive_sync.partition_extractor_class",
+      "org.apache.hudi.hive.MultiPartKeysValueExtractor"
+    )
+    .mode(SaveMode.Append)
+    .save(basePath)
 
-  data.show(10)
-
-  // val verificationResult = VerificationSuite()
-  //   .onData(data)
-  //   .addCheck(
-  //     Check(CheckLevel.Error, "unit testing my data")
-  //       .hasSize(_ == 5) // we expect 5 rows
-  //       .isComplete("id") // should never be NULL
-  //       .isUnique("id") // should not contain duplicates
-  //       .isComplete("productName") // should never be NULL
-  //       // should only contain the values "high" and "low"
-  //       .isContainedIn("priority", Array("high", "low"))
-  //       .isNonNegative("numViews") // should not contain negative values
-  //       // at least half of the descriptions should contain a url
-  //       .containsURL("description", _ >= 0.5)
-  //       // half of the items should have less than 10 views
-  //       .hasApproxQuantile("numViews", 0.5, _ <= 10)
-  //   )
-  //   .run()
-
-  // if (verificationResult.status == CheckStatus.Success) {
-  //   println("The data passed the test, everything is fine!")
-  // } else {
-  //   println("Dws data quality:\n")
-
-  //   val resultsForAllConstraints = verificationResult.checkResults
-  //     .flatMap { case (_, checkResult) => checkResult.constraintResults }
-
-  //   resultsForAllConstraints
-  //     .filter { _.status != ConstraintStatus.Success }
-  //     .foreach { result =>
-  //       println(s"${result.constraint}: ${result.message.get}")
-  //     }
-  // }
   spark.sparkContext.stop()
 }
